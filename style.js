ed24b2689188cef351caad21824ca952ef0f846b
pfnas <103249634+pfnas@users.noreply.github.com>
2022-10-19 17:16:15 +0200

    changement du logo, modification du footer

--- style.js
+++ style.js
@@ -1,28 +1,32 @@
-const slider = document.querySelector('.slideshowcontainer')
-let holding = false;
-let firstClickX;
-let alreadyLeftscrolled;
+// const slider = document.querySelector(".slideshowcontainer");
+// let holding = false;
+// let firstClickX;
+// let alreadyLeftscrolled;
 
-slider.addEventListener('mousedown', e =>{
-    holding = true;
-    firstClickX = e.pageX - slider.offsetLeft;
-    alreadyLeftscrolled = slider.scrollLeft;
+// slider.addEventListener("mousedown", (e) => {
+//   holding = true;
+//   firstClickX = e.pageX - slider.offsetLeft;
+//   alreadyLeftscrolled = slider.scrollLeft;
+// });
+// slider.addEventListener("mousemove", (e) => {
+//   if (!holding) return;
+//   const X = e.pageX - slider.offsetLeft;
+//   const scrolled = x - firstClickX;
+//   slider.scrollLeft = alreadyLeftscrolled - scrolled;
+//   console.log(alreadyLeftscrolled - scrolled);
+// });
+// slider.addEventListener("mouseup", () => {
+//   holding = false;
+// });
+// slider.addEventListener("mouseleave", () => {
+//   holding = false;
+// });
+// window.dataLayer = window.dataLayer || [];
+// function gtag() {
+//   dataLayer.push(arguments);
+// }
+// gtag("js", new Date());
 
-})
-slider.addEventListener('mousemove', e =>{
-    if(!holding) return;
-    const X = e.pageX - slider.offsetLeft;
-    const scrolled = (x - firstClickX);
-    slider.scrollLeft = alreadyLeftscrolled - scrolled;
-    console.log(alreadyLeftscrolled - scrolled);
+// gtag("config", "G-P3MY8DNWV7");
 
-
-})
-slider.addEventListener('mouseup', () =>{
-    holding = false;
-
-})
-slider.addEventListener('mouseleave', () =>{
-    holding = false;
-
-})
+// //  <div id="infos">infos pratiques <div>
