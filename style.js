c90e36bfceab0c5b34a9c63f8bf770e78a39bbe7
pfnas <103249634+pfnas@users.noreply.github.com>
2022-10-25 16:02:12 +0200

    modification de l'input et du carosusel en page d'accueil

--- style.js
+++ style.js
@@ -1,32 +1,27 @@
-// const slider = document.querySelector(".slideshowcontainer");
-// let holding = false;
-// let firstClickX;
-// let alreadyLeftscrolled;
-
-// slider.addEventListener("mousedown", (e) => {
-//   holding = true;
-//   firstClickX = e.pageX - slider.offsetLeft;
-//   alreadyLeftscrolled = slider.scrollLeft;
-// });
-// slider.addEventListener("mousemove", (e) => {
-//   if (!holding) return;
-//   const X = e.pageX - slider.offsetLeft;
-//   const scrolled = x - firstClickX;
-//   slider.scrollLeft = alreadyLeftscrolled - scrolled;
-//   console.log(alreadyLeftscrolled - scrolled);
-// });
-// slider.addEventListener("mouseup", () => {
-//   holding = false;
-// });
-// slider.addEventListener("mouseleave", () => {
-//   holding = false;
-// });
-// window.dataLayer = window.dataLayer || [];
-// function gtag() {
-//   dataLayer.push(arguments);
-// }
-// gtag("js", new Date());
-
-// gtag("config", "G-P3MY8DNWV7");
-
-// //  <div id="infos">infos pratiques <div>
+const items = document.querySelectorAll(".card_competence");
+const nbSlide = items.length;
+const suivant = document.querySelector(".right");
+const precedent = document.querySelector(".left");
+let count = 0;
+function slidesuivant() {
+  items[count].classList.remove("active");
+  if (count < nbSlide - 1) {
+    count++;
+  } else {
+    count = 0;
+  }
+  items[count].classList.add("active");
+  console.log(count);
+}
+suivant.addEventListener("click", slidesuivant);
+function slideprecedente() {
+  items[count].classList.remove("active");
+  if (count > 0) {
+    count--;
+  } else {
+    count = nbSlide - 1;
+  }
+  items[count].classList.add("active");
+  console.log(count);
+}
+precedent.addEventListener("click", slideprecedente);
